ab556b0614e785c5db7f7b01b467392fd0e9ae19
Owoa <41863894+Owoa@users.noreply.github.com>
2022-01-25 19:09:37 +0100

    Add files via upload

---
+++ C42/C02_2retries/ex01/ft_strncpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfricard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 22:09:06 by dfricard          #+#    #+#             */
+/*   Updated: 2022/01/20 14:07:00 by dfricard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/*
+#include<stdio.h>
+
+int main()
+{
+	char src[20] = "Hey";
+	char dest[20];
+
+	printf("%s",ft_strncpy(dest, src, 10));
+}
+*/
