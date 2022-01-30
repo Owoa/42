7307fa12cf8d30eaed647d806aca1e5855ca7825
Owoa <41863894+Owoa@users.noreply.github.com>
2022-01-30 19:00:50 +0100

    celui la est ok !

---
+++ C42/C07/ex01/ft_range.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfricard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 18:03:33 by dfricard          #+#    #+#             */
+/*   Updated: 2022/01/30 18:11:10 by dfricard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int i;
+	int *tab;
+
+	i = 0;	
+	if (min >= max)
+		return (NULL);
+	tab = malloc(sizeof(*tab) *(max - min));
+	while (min < max)
+	{
+		tab[i] = min;
+		min++;
+		i++;
+	}
+	return (tab);
+}
+
+int main()
+{
+	int *cc = ft_range(2 , 4);
+	int i = 0;
+	if (cc != NULL)
+	{
+		while (i < 2)
+		{
+	        	printf("%d",cc[i]);
+			i++;
+		}
+	}
+	free(cc);
+}
